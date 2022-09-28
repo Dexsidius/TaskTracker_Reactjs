337cc9d121c3aa6083d137d24fff5bbafe44044b
DaShawn Larry <dashawnlarry@gmail.com>
2022-09-27 23:55:06 -0400

    Setting Up react and creating task tracker

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+
+const App = () => {
+  return (                        //Declaring a prop on class 'Header'
+    <div className = 'container'>
+      <Header />      
+    </div>
+  )
+}
+
+export default App
